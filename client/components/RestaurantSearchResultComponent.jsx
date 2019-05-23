7ad8e0c9533d2c06a294042bb818739fdd67a526
Alena Budzko <budzko.alena@gmail.com>
2019-05-23 17:40:26 -0400

    Added style to search component

--- client/components/RestaurantSearchResultComponent.jsx
+++ client/components/RestaurantSearchResultComponent.jsx
@@ -3,10 +3,67 @@ import { gql } from 'apollo-boost';
 import { graphql, compose, ApolloConsumer } from 'react-apollo';
 import { joesFrontEndCookieParser } from '../services/authenticate';
 import { exGetLikes } from './../containers/HistoryContainer';
-
+import styled from 'styled-components';
 
 const myCookies = joesFrontEndCookieParser(document.cookie);
 const myUserId = myCookies.userId;
+
+const Information = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  margin: 10px;
+  background: radial-gradient(circle, rgba(255,199,0,0.8377120206489675) 0%, rgba(235,253,29,0.5486264749262537) 53%, rgba(252,176,69,0.11204830383480824) 100%);
+  font-family: Helvetica;
+  border: 1px solid #f2f2f2;
+  padding: 14px;
+  font-color: #333333;
+  transition: 0.4s;
+
+  :hover {
+    box-shadow: 1px 0px 10px #4eb5f1;
+    transform: scale(1.01);
+  }
+`
+const restName = styled.p`
+  font-size: 28px;
+  font-weight: 700;
+  margin-bottom: 15px;
+`
+const restAddress = styled.p`
+  font-syze: 24px;
+  font-weight: 600;
+`
+
+const restPrice = styled.p`
+  font-syze: 24px;
+  font-weight: 600;
+`
+
+const restRating = styled.p`
+  font-syze: 24px;
+  font-weight: 600;
+`
+
+const LikeButton = styled.button`
+  display:inline-block;
+   padding:0.3em 1.2em;
+   margin:0 0.3em 0.3em 0;
+   border-radius:2em;
+   height: 20px;
+   width: 50px;
+   text-decoration:none;
+   font-family:'Roboto',sans-serif;
+   font-weight:300;
+   color:#FFFFFF;
+   background-color:#4eb5f1;
+   text-align:center;
+   transition: all 0.2s;
+
+  hover: {
+     background-color:#4095c6;
+  }
+`
 
 const AddRestaurantMutation = gql`
   mutation ($rating: Float!, $review_count: Int!, $yelp_id: String!, $name: String!, $display_address: String!, $image_url: String!, $url: String!, $price: String!, $latitude: Float!, $longitude: Float!, $user_id: Int!){
@@ -43,21 +100,31 @@ const RestaurantSearchResultComponent = (props) => {
 
   props.data.user_id = parseInt(myUserId);
   return (
-    <div className="searchResult">
-      <h4>{props.data.name}</h4>
-      <h6>Address: {props.data.display_address}</h6>
-      <h6>Price: {props.data.price}</h6>
-      <h6>Rating: {props.data.rating}</h6>
-      <h6>Review count: {props.data.review_count}</h6>
-      <button onClick={() => {
+    <Information>
+      <restName><b>{props.data.name}</b></restName>
+      <restAddress>
+        <b>Address: </b>{props.data.display_address}
+      </restAddress>
+      <restPrice>
+      <b>Price: </b>{props.data.price}
+      </restPrice>
+      <restRating>
+      <b>Rating: </b>{props.data.rating}
+      </restRating>
+      <restReview>
+      <b>Review count: </b>{props.data.review_count}
+      </restReview>
+
+      <LikeButton onClick={() => {
         props.AddRestaurantMutation({
           variables: props.data,
           // refetch: [{ query: getLikesQuery }]
         }, location.reload());
       }}>
       Like
-      </button>
-    </div>);
+      </LikeButton>
+    </Information>
+  )
 }
 
 export default compose(
